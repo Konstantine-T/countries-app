d9d1fef9e4a5294c17fd4bec5404f83ae90d1e84
headson <kostatavadze@gmail.com>
2024-10-17 12:50:08 +0400

    made form input fields controlled and added validations on some of them

--- src/pages/contact/components/Form.tsx
+++ src/pages/contact/components/Form.tsx
@@ -1,46 +1,93 @@
+import { useState } from "react";
 import styles from "./Form.module.css";
 
 const Form: React.FC = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const target = e.target as HTMLFormElement;
-
     const data = {
-      firstName: target.firstName.value,
-      lastName: target.lastName.value,
-      email: target.email.value,
-      message: target.message.value,
+      firstName,
+      lastName,
+      email,
+      message,
     };
 
     console.log(data);
 
-    target.firstName.value = "";
-    target.lastName.value = "";
-    target.email.value = "";
-    target.message.value = "";
+    setFirstName("");
+    setLastname("");
+    setEmail("");
+    setMessage("");
+  };
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.target.id === "firstName") {
+      setFirstName(e.target.value);
+      if (e.target.value.length > 10) {
+        e.target.style.color = "red";
+        alert("first name can't be longer than 10 characters");
+      }
+      e.target.style.color = "white";
+    }
+    if (e.target.id === "lastName") {
+      setLastname(e.target.value);
+      if (e.target.value.length > 10) {
+        e.target.style.color = "red";
+        alert("last name can't be longer than 10 characters");
+      }
+      e.target.style.color = "white";
+    }
+    if (e.target.id === "email") {
+      setEmail(e.target.value);
+    }
+    if (e.target.id === "message") {
+      setMessage(e.target.value);
+    }
   };
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.formGroup}>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" id="firstName" name="firstName" />
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName" />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" name="email" onChange={handleChange} />
       </div>
 
       <div className={styles.formGroup}>
         <label htmlFor="message">Message</label>
-        <textarea id="message" name="message"></textarea>
+        <textarea
+          id="message"
+          name="message"
+          onChange={handleChange}
+        ></textarea>
       </div>
 
       <button type="submit" className={styles.submitButton}>
--- src/pages/home/components/Form/AddCountry.tsx
+++ src/pages/home/components/Form/AddCountry.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Country } from "../../reducer/reducer";
 import styles from "./AddCountry.module.css";
 
@@ -6,17 +7,21 @@ interface AddCountryProps {
 }
 
 const AddCountry: React.FC<AddCountryProps> = ({ onAddCountry }) => {
+  const [name, setName] = useState("");
+  const [capital, setCapital] = useState("");
+  const [population, setPopulation] = useState("");
+  const [area, setArea] = useState("");
+  const [description, setDescription] = useState("");
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const target = e.target as HTMLFormElement;
-
     const newCountry: Country = {
-      name: target.countryName.value,
-      capital: target.capitalCity.value,
-      population: target.population.value,
-      area: target.area.value,
-      description: target.message.value,
+      name,
+      capital,
+      population,
+      area,
+      description,
       likes: 0,
       id: String(Math.random()),
       isDeleted: false,
@@ -24,38 +29,89 @@ const AddCountry: React.FC<AddCountryProps> = ({ onAddCountry }) => {
 
     onAddCountry(newCountry);
 
-    target.countryName.value = "";
-    target.capitalCity.value = "";
-    target.population.value = "";
-    target.area.value = "";
-    target.message.value = "";
+    setName("");
+    setCapital("");
+    setPopulation("");
+    setArea("");
+    setDescription("");
+  };
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.target.id === "countryName") {
+      setName(e.target.value);
+      if (e.target.value.length > 15) {
+        e.target.style.color = "red";
+        alert("Country name can't be longer than 15 characters");
+      }
+      e.target.style.color = "white";
+    }
+    if (e.target.id === "capitalCity") {
+      setCapital(e.target.value);
+      if (e.target.value.length > 15) {
+        e.target.style.color = "red";
+        alert("Capital city name can't be longer than 15 characters");
+      }
+      e.target.style.color = "white";
+    }
+    if (e.target.id === "population") {
+      setPopulation(e.target.value);
+    }
+    if (e.target.id === "area") {
+      setArea(e.target.value);
+    }
+    if (e.target.id === "message") {
+      setDescription(e.target.value);
+    }
   };
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.formGroup}>
         <label>Country name</label>
-        <input type="text" id="countryName" name="countryName" />
+        <input
+          type="text"
+          id="countryName"
+          name="countryName"
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label>Capital City</label>
-        <input type="text" id="capitalCity" name="capitalCity" />
+        <input
+          type="text"
+          id="capitalCity"
+          name="capitalCity"
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label>Population</label>
-        <input type="text" id="population" name="population" />
+        <input
+          type="text"
+          id="population"
+          name="population"
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label>Area</label>
-        <input type="text" id="area" name="area" />
+        <input type="text" id="area" name="area" onChange={handleChange} />
       </div>
 
       <div className={styles.formGroup}>
         <label htmlFor="message">Additional info</label>
-        <textarea id="message" name="message"></textarea>
+        <textarea
+          id="message"
+          name="message"
+          onChange={handleChange}
+        ></textarea>
       </div>
 
       <button type="submit" className={styles.submitButton}>
